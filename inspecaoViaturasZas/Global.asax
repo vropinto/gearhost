d1108bc5bb10a0da96195139908e966201e2a49d
vropinto <vropinto71@gmail.com>
2016-10-16 23:27:23 +0100

    first attempt

---
+++ inspecaoViaturasZas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="inspecaoViaturasZas.MvcApplication" Language="C#" %>
---
+++ inspecaoViaturasZas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(inspecaoViaturasZas.Startup))]
+namespace inspecaoViaturasZas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
